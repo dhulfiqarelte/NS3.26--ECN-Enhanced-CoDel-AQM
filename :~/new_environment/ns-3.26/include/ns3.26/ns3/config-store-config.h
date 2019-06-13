ba7fc07daffa7d72507f34e8e475e3af0aa50492
dhulfiqarelte <adozan@eszakigrid105.inf.elte.hu>
2019-06-13 15:40:31 +0200

    NS-3.26 CoDel ECN

---
+++ :~/new_environment/ns-3.26/include/ns3.26/ns3/config-store-config.h
@@ -0,0 +1,12 @@
+/* WARNING! All changes made to this file will be lost! */
+
+#ifndef W_NS3_CONFIG_STORE_CONFIG_H_WAF
+#define W_NS3_CONFIG_STORE_CONFIG_H_WAF
+
+#define PYTHONDIR "/home/adozan/new_environment/ns-3.26/:~/new_environment/ns-3.26/build/lib/python2.7/site-packages"
+#define PYTHONARCHDIR "/home/adozan/new_environment/ns-3.26/:~/new_environment/ns-3.26/build/lib/python2.7/site-packages"
+#define HAVE_PYEMBED 1
+#define HAVE_PYEXT 1
+#define HAVE_PYTHON_H 1
+
+#endif /* W_NS3_CONFIG_STORE_CONFIG_H_WAF */
